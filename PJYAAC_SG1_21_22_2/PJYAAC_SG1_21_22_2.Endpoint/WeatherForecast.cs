522fe2a153d8cec663e6023d6954c7e26880e872
Kristóf <z.kristof97@gmail.com>
2022-02-23 18:52:59 +0100

    first commit

---
+++ PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.Endpoint/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace PJYAAC_SG1_21_22_2.Endpoint
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}

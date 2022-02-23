d359d0d318fef1ccbb8df9d37e8df629a08468e0
Kristóf <z.kristof97@gmail.com>
2022-02-23 19:08:21 +0100

    add Bicycle entity class

--- PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PJYAAC_SG1_21_22_2.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.Models/Entities/Bicycle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PJYAAC_SG1_21_22_2.Models.Entities
+{
+    public class Bicycle
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public DateTime DateOfPurchase { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+        public bool IsFullSuspension { get; set; }
+        public bool IsElectric { get; set; }
+        public string Color { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Type { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Model { get; set; }
+    }
+}

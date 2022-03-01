142c6718cfeb749745c51d4d52ef7791f228bb3c
Kristóf <z.kristof97@gmail.com>
2022-03-01 21:42:51 +0100

    display bikes in a list

---
+++ PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.WpfClient/BicycleVM.cs
@@ -0,0 +1,48 @@
+﻿using GalaSoft.MvvmLight;
+using PJYAAC_SG1_21_22_2.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJYAAC_SG1_21_22_2.WpfClient
+{
+    public class BicycleVM : ViewModelBase
+    {
+        private Bicycle bicycle;
+
+        public Bicycle Bicycle
+        {
+            get { return bicycle; }
+            set { Set(ref bicycle, value); }
+        }
+
+        public ObservableCollection<Bicycle> Bikes { get; set; }
+
+        public BicycleVM()
+        {
+            Bikes = new ObservableCollection<Bicycle>();
+            
+            if(IsInDesignMode)
+            {
+                var bicycle = new Bicycle()
+                {
+                    Id = 10
+                    , Color = "Fehér"
+                    , DateOfPurchase = DateTime.Now
+                    , IsElectric = false
+                    , IsFullSuspension = true
+                    , Model = "Specialized"
+                    , Price = 50
+                    , Type = "Trekking"
+                };
+
+                Bikes.Add(bicycle);
+                Bikes.Add(bicycle);
+                Bikes.Add(bicycle);
+            }
+        }
+    }
+}

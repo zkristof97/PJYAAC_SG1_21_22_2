a7921ce67d16f3b2c8805d374d0c9899bce06897
Kristóf <z.kristof97@gmail.com>
2022-03-05 11:25:32 +0100

    add buttons without functionality

--- PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.WpfClient/MainWindow.xaml.cs
+++ PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.WpfClient/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,32 @@ namespace PJYAAC_SG1_21_22_2.WpfClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BicycleVM vm;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CreateClicked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void EditClicked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void DeleteClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(Resources["VM"] != null)
+            {
+                vm = (BicycleVM)Resources["VM"];
+            }
         }
     }
 }

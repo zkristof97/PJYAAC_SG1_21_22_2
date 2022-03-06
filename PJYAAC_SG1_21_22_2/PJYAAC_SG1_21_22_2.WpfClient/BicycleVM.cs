045d87717518932d3d62f89f2bcf78a075fa56a6
Kristóf <z.kristof97@gmail.com>
2022-03-06 12:14:46 +0100

    use BicycleModel instead of entity

--- PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.WpfClient/BicycleVM.cs
+++ PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.WpfClient/BicycleVM.cs
@@ -1,5 +1,5 @@
 ﻿using GalaSoft.MvvmLight;
-using PJYAAC_SG1_21_22_2.Models.Entities;
+using PJYAAC_SG1_21_22_2.WpfClient.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -11,37 +11,64 @@ namespace PJYAAC_SG1_21_22_2.WpfClient
 {
     public class BicycleVM : ViewModelBase
     {
-        private Bicycle bicycle;
+        private BicycleModel bicycle;
 
-        public Bicycle Bicycle
+        public BicycleModel Bicycle
         {
             get { return bicycle; }
             set { Set(ref bicycle, value); }
         }
 
-        public ObservableCollection<Bicycle> Bikes { get; set; }
+        private ObservableCollection<BicycleModel> bikes;
+
+        public ObservableCollection<BicycleModel> Bikes
+        {
+            get { return bikes; }
+            set { Set(ref bikes, value); }
+        }
 
         public BicycleVM()
         {
-            Bikes = new ObservableCollection<Bicycle>();
-            
+            Bikes = new ObservableCollection<BicycleModel>();
+
             if(IsInDesignMode)
             {
-                var bicycle = new Bicycle()
-                {
-                    Id = 10
-                    , Color = "Fehér"
-                    , DateOfPurchase = DateTime.Now
-                    , IsElectric = false
-                    , IsFullSuspension = true
-                    , Model = "Specialized"
-                    , Price = 50
-                    , Type = "Trekking"
-                };
 
-                Bikes.Add(bicycle);
-                Bikes.Add(bicycle);
-                Bikes.Add(bicycle);
+                var bike1 = new BicycleModel(
+                1
+                , DateTime.Now
+                , 100000
+                , false
+                , false
+                , "white"
+                , "Specialized"
+                , "Trekking");
+
+            var bike2 = new BicycleModel(
+                2
+               , DateTime.Now
+               , 200000
+               , false
+               , false
+               , "red"
+               , "Cube"
+               , "Mountain Bike");
+
+            var bike3 = new BicycleModel(
+                3
+               , DateTime.Now
+               , 800000
+               , false
+               , false
+               , "Blue"
+               , "Cube"
+               , "Mountain Bike");
+
+            Bikes.Add(bike1);
+            Bikes.Add(bike2);
+            Bikes.Add(bike3);
+
+            Bicycle = bike2;
             }
         }
     }
---
+++ PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.WpfClient/Models/BicycleModel.cs
@@ -0,0 +1,108 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJYAAC_SG1_21_22_2.WpfClient.Models
+{
+    public class BicycleModel : ObservableObject, ICloneable
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { Set(ref id, value); }
+        }
+
+
+        private DateTime dateOfPurchase;
+
+        public DateTime DateOfPurchase
+        {
+            get { return dateOfPurchase; }
+            set { Set(ref dateOfPurchase, value); }
+        }
+
+        private int price;
+
+        public int Price
+        {
+            get { return price; }
+            set { Set(ref price, value); }
+        }
+
+        private bool isFullSuspension;
+
+        public bool IsFullSuspension
+        {
+            get { return isFullSuspension; }
+            set { Set(ref isFullSuspension, value); }
+        }
+
+        private bool isElectric;
+
+        public bool IsElectric
+        {
+            get { return isElectric; }
+            set { Set(ref isElectric, value); }
+        }
+
+        private string color;
+
+        public string Color
+        {
+            get { return color; }
+            set { Set(ref color, value); }
+        }
+
+        private string type;
+
+        public string Type
+        {
+            get { return type; }
+            set { Set(ref type, value); }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { Set(ref model, value); }
+        }
+
+        public BicycleModel()
+        {
+
+        }
+
+        public BicycleModel(int id, DateTime dateOfPurchase, int price, bool isFullSuspension, bool isElectric, string color, string type, string model)
+        {
+            this.id = id;
+            this.dateOfPurchase = dateOfPurchase;
+            this.price = price;
+            this.isFullSuspension = isFullSuspension;
+            this.isElectric = isElectric;
+            this.color = color;
+            this.type = type;
+            this.model = model;
+        }
+
+        public object Clone()
+        {
+            return new BicycleModel(
+                Id,
+                DateOfPurchase,
+                Price,
+                IsFullSuspension,
+                IsElectric,
+                Color,
+                Type,
+                Model
+            );
+        }
+    }
+}

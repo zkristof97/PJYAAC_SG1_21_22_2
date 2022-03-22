a1cc900ce30cfb7d2c2cae2eae82b347b38b6dde
Kristóf <z.kristof97@gmail.com>
2022-03-22 06:37:08 +0100

    move VM to new folder

--- PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.WpfClient/ViewModels/BicycleVM.cs
+++ PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.WpfClient/ViewModels/BicycleVM.cs
@@ -11,7 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace PJYAAC_SG1_21_22_2.WpfClient
+namespace PJYAAC_SG1_21_22_2.WpfClient.ViewModels
 {
     public class BicycleVM : ViewModelBase
     {

a54c6a6b333581cfab6f7ab9c387a5d7cbea712e
Kristóf <z.kristof97@gmail.com>
2022-03-25 07:17:10 +0100

    Add validation

--- PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.Logic/Services/BicycleLogic.cs
+++ PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.Logic/Services/BicycleLogic.cs
@@ -25,7 +25,7 @@ namespace PJYAAC_SG1_21_22_2.Logic.Services
         {
             var allBikes = ReadAll();
 
-            var existingBike = allBikes.First((bicycle) => bicycle.Model == entity.Model);
+            var existingBike = allBikes.FirstOrDefault((bicycle) => bicycle.Model == entity.Model);
 
             if(existingBike != null)
             {
@@ -35,6 +35,39 @@ namespace PJYAAC_SG1_21_22_2.Logic.Services
 
             return 0;
         }
+
+        private void RequiredValidator(Bicycle entity, string field)
+        {
+            var prop = typeof(Bicycle).GetProperty(field);
+            var propType = prop.PropertyType;
+            object value = prop.GetValue(entity);
+
+            if (propType == typeof(string) && string.IsNullOrWhiteSpace(value as string) || propType == typeof(int) && (int)value <= 0)
+            {
+                throw new Exception($"{field} is a required field.");
+            } 
+        }
+
+        private void StringLengthValidator(Bicycle entity, string field, int length = 50)
+        {
+            string value = typeof(Bicycle).GetProperty(field).GetValue(entity) as string;
+            if (!string.IsNullOrWhiteSpace(value) && value.Length > length)
+            {
+                throw new Exception($"{field} cannot be longer than {length} characters.");
+            }
+        }
+
+        private void ValidateFields(Bicycle entity)
+        {
+            RequiredValidator(entity, nameof(entity.Model));
+            StringLengthValidator(entity, nameof(entity.Model));
+
+            RequiredValidator(entity, nameof(entity.Type));
+            StringLengthValidator(entity, nameof(entity.Type));
+
+            RequiredValidator(entity, nameof(entity.Price));
+        }
+
         public IList<Bicycle> ReadAll()
         {
             return _bicycleRepository.ReadAll().ToList();
@@ -47,9 +80,11 @@ namespace PJYAAC_SG1_21_22_2.Logic.Services
 
         public Bicycle Create(Bicycle entity)
         {
-            if(BicycleIdWithSameModel(entity) != 0)
+            ValidateFields(entity);
+
+            if (BicycleIdWithSameModel(entity) != 0)
             {
-                throw new Exception("A bicycle with this modell already exists!");
+                throw new Exception("A bicycle with this model already exists!");
             }
 
             var result = _bicycleRepository.Create(entity);
@@ -59,12 +94,12 @@ namespace PJYAAC_SG1_21_22_2.Logic.Services
 
         public Bicycle Update(Bicycle entity)
         {
-            // TODO check access
+            ValidateFields(entity);
 
             var bicycleId = BicycleIdWithSameModel(entity);
-            if (bicycleId != entity.Id)
+            if (bicycleId != 0 && bicycleId != entity.Id)
             {
-                throw new Exception("A bicycle with this modell already exists!");
+                throw new Exception("A bicycle with this model already exists!");
             }
 
             var result = _bicycleRepository.Update(entity);

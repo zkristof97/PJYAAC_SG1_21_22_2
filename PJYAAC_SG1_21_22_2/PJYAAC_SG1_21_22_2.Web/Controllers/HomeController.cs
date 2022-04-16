faef6fb115ef6571e1042771dc1648b955f87114
Kristóf <z.kristof97@gmail.com>
2022-04-16 17:27:31 +0200

    cleanup

--- PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.Web/Controllers/HomeController.cs
+++ PJYAAC_SG1_21_22_2/PJYAAC_SG1_21_22_2.Web/Controllers/HomeController.cs
@@ -23,12 +23,7 @@ namespace PJYAAC_SG1_21_22_2.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+             [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
